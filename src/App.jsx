97f8c979cbc57151fca99f5b933717047226d67b
Mahin Khan <mdmahinkhan621@gmail.com>
2026-02-18 15:34:34 +0600

    2:37

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import ResultGrid from "./components/ResultGrid"
 import SearchBar from "./components/SearchBar"
+import Tabs from "./components/Tabs"
 
 const App = () => {
 
   return (
     <div className='min-h-screen w-full flex bg-[#94a811] items-center justify-center'>
       <SearchBar></SearchBar>
+
+      <Tabs></Tabs>
+
+      <ResultGrid></ResultGrid>
     </div>
   )
 }
